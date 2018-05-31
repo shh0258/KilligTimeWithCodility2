6fc454bc4e7e1faf12bd8d1407e01a08a7a558e9
shh0258 <big0258@naver.com>
2018-05-31 10:15:04 +0900

    180531

---
+++ KillingTimeWithCodility2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  KillingTimeWithCodility2
+//
+//  Created by user on 2018. 5. 31..
+//  Copyright © 2018년 han. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
